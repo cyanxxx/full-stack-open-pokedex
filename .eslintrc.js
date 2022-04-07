50e889cf6a4d07a600189c7dcbc88d1af3f41d92
cyanxxx <504512494@qq.com>
2022-04-07 18:25:58 +0800

    update: 11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    'node': true,
+    "node": true,
     "cypress/globals": true
   },
   "extends": [
@@ -18,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
